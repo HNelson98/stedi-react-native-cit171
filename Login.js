353d0176847e9b77bb05d46ce9de7e1170b3f2a9
Henry Nelson <60790820+HNelson98@users.noreply.github.com>
2022-06-18 17:34:02 -0600

    added texting otp

--- Login.js
+++ Login.js
@@ -1,5 +1,16 @@
 import {useState} from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { TouchableOpacity, SafeAreaView, StyleSheet, TextInput, Text } from "react-native";
+
+
+const sendText = async (phoneNumber) => {
+  // using fetch do a POST to https://dev.stedi.me/twofactorlogin
+  await fetch("https://dev.stedi.me/twofactorlogin/" + phoneNumber, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/text"
+  }
+});
+}
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -21,6 +32,18 @@ const Login = () => {
         keyboardType="numeric"
         secureTextEntry={true}
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{sendText(phoneNumber)}}
+      >
+        <Text>Get OTP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{sendText(phoneNumber)}}
+      >
+        <Text>Login</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -30,10 +53,21 @@ const styles = StyleSheet.create({
     height: 40,
     margin: 12,
     borderWidth: 1,
+    borderRadius: 5,
     padding: 10,
   },
   mainView:{
     marginTop: 100
+  },
+  button:{
+    alignItems:"center",
+    width: 100,
+    margin: 10,
+    borderwidth: 1,
+    borderRadius: 20, 
+    padding: 10,
+    backgroundColor: "#DDDD",
+    alignSelf: "center",
   }
 });
 
